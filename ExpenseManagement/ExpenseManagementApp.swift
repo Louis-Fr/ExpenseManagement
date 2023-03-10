33012b14a20172d0885a45c677fb63fbaef7a132
Louis Rey <louisreypro@yahoo.com>
2023-03-10 12:41:12 +0100

    Initial Commit

---
+++ ExpenseManagement/ExpenseManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseManagementApp.swift
+//  ExpenseManagement
+//
+//  Created by Louis REY on 10/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
