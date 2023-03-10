adc0e7d067614fa09ec850fefbe8a21e5b1eae92
Louis Rey <louisreypro@yahoo.com>
2023-03-10 12:46:49 +0100

    test modify View

--- ExpenseManagement/ContentView.swift
+++ ExpenseManagement/ContentView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack {
+                //Image
+            VStack {
+                Text("Paypal")
+                Text("Solde : 264 €")
+            }
         }
-        .padding()
     }
 }
 
