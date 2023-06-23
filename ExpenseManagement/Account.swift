2b66d7f2064e84ab817c4f03524df6c9b52725e1
Louis Rey <louisreypro@yahoo.com>
2023-06-23 13:09:44 +0200

    add file account for data model

---
+++ ExpenseManagement/Account.swift
@@ -0,0 +1,18 @@
+//
+//  Account.swift
+//  ExpenseManagement
+//
+//  Created by Louis REY on 23/06/2023.
+//
+
+import Foundation
+
+/// Objet Account qui encapsule 3 données
+
+struct Account {
+    
+    let nameIcon: String
+    let name: String
+    let amount: Float
+    
+}
---
+++ ExpenseManagement/AccountCell.swift
@@ -0,0 +1,20 @@
+//
+//  AccountCell.swift
+//  ExpenseManagement
+//
+//  Created by Louis REY on 19/06/2023.
+//
+
+import SwiftUI
+
+struct AccountCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AccountCell_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountCell()
+    }
+}
