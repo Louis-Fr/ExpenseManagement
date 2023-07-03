ae8fbf05e2979092669ae2c6c69c5bcaf258067b
Louis Rey <louisreypro@yahoo.com>
2023-07-03 12:09:18 +0200

    Animation of selection mode and symbols

--- ExpenseManagement/CurrencySelector.swift
+++ ExpenseManagement/CurrencySelector.swift
@@ -20,11 +20,22 @@ struct CurrencySelector: View {
     
     var body: some View {
         ZStack {
+            /// Retourne l'ensemble des indices de currencies (0,1,2,3,4)
             ForEach(currencies.indices) { index in
+                /// 
                 Image(systemName: selectedCurrency == currencies[index] ? currencies[index].filledIconName : currencies[index].iconName)
                     .font(.system(size: 32))
                     .offset(x: isSelectedMode ? -CGFloat(index) * 40 : 0)
                     .opacity(selectedCurrency == currencies[index] || isSelectedMode ? 1 : 0)
+                    .onTapGesture {
+                        if isSelectedMode {
+                            selectedCurrency = currencies[index]
+                        }
+                        withAnimation(.easeInOut(duration: 0.2)) {
+                            isSelectedMode.toggle()
+                        }
+                        
+                    }
             }
         }
     }
